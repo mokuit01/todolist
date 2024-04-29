65e337af8dd71fba8c15a41dd102eb437e57cd86
Jakobs <122310617+AliJakobs@users.noreply.github.com.>
2024-04-29 10:42:10 +0200

    start with todo app

--- fronted/src/routes/+page.svelte
+++ fronted/src/routes/+page.svelte
@@ -1,59 +1,51 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+    /**
+	 * @type {any[]}
+	 */
+    let todos = [];
+    let newTodo = '';
+
+    function addTodo() {
+        if (newTodo) {
+            todos = [...todos, newTodo];
+            newTodo = '';
+        }
+    }
+
+    /**
+	 * @param {number} index
+	 */
+    function removeTodo(index) {
+        todos = todos.filter((_, i) => i !== index);
+    }
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+    <title>Todo App</title>
+    <meta name="description" content="Svelte Todo app" />
 </svelte:head>
 
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
+    <h1>Todo App</h1>
 
-		to your new<br />SvelteKit app
-	</h1>
+    <input bind:value={newTodo} placeholder="New todo" />
+    <button on:click={addTodo}>Add</button>
 
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
+    <ul>
+        {#each todos as todo, index}
+            <li>
+                {todo} <button on:click={() => removeTodo(index)}>Remove</button>
+            </li>
+        {/each}
+    </ul>
 </section>
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
+    section {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        flex: 0.6;
+    }
 </style>
--- fronted/src/routes/Header.svelte
+++ fronted/src/routes/Header.svelte
@@ -1,15 +1,8 @@
 <script>
 	import { page } from '$app/stores';
-	import logo from '$lib/images/svelte-logo.svg';
-	import github from '$lib/images/github.svg';
 </script>
 
 <header>
-	<div class="corner">
-		<a href="https://kit.svelte.dev">
-			<img src={logo} alt="SvelteKit" />
-		</a>
-	</div>
 
 	<nav>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
@@ -17,7 +10,7 @@
 		</svg>
 		<ul>
 			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/">Home</a>
+				<a href="/">Todolist</a>
 			</li>
 			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
 				<a href="/about">About</a>
@@ -30,12 +23,6 @@
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
 		</svg>
 	</nav>
-
-	<div class="corner">
-		<a href="https://github.com/sveltejs/kit">
-			<img src={github} alt="GitHub" />
-		</a>
-	</div>
 </header>
 
 <style>
